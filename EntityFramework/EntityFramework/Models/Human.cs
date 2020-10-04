d9efcad133239c2054124cbf7a7a13002e76f9d7
anassbb <anasabassi11@gmail.com>
2020-10-04 16:03:07 +0200

    relation one to many

---
+++ EntityFramework/EntityFramework/Models/Human.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework.Models
+{
+    class Human
+    {
+        public int HumanId { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Car> Cars { get; set; }
+    }
+}
