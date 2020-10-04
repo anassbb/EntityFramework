313e603bf2b5785777931b0ed096c958c1c663ef
anassbb <anasabassi11@gmail.com>
2020-10-04 11:26:00 +0200

    Initiation du entity framework

---
+++ EntityFramework/EntityFramework/Test/TestDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework.Test
+{
+    class TestDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            SqlConnectionStringBuilder ConnectionString = new SqlConnectionStringBuilder()
+            {
+                DataSource = @".",
+                InitialCatalog = "Test",
+                IntegratedSecurity = true
+            };
+
+            optionsBuilder.UseSqlServer(ConnectionString.ToString());
+        }
+
+    }
+}
