aefb494dbd7b6d234c0a0c41437228273014439f
anassbb <anasabassi11@gmail.com>
2020-10-04 17:47:09 +0200

    Where select with linq to sql

--- EntityFramework/EntityFramework/Program.cs
+++ EntityFramework/EntityFramework/Program.cs
@@ -12,6 +12,11 @@ namespace EntityFramework
         {
             using (var db = new Models.TestDbContext())
             {
+                Etudaint student = db.Etudiants.Where(x => x.Id == 1).SingleOrDefault();
+
+                Console.WriteLine(student.Name);
+
+                Console.ReadLine();
                 //var Students = db.Etudiants.ToList();
 
                 //foreach (var item in Students)
@@ -26,14 +31,12 @@ namespace EntityFramework
                 //    Console.WriteLine(item.Name);
                 //}
 
-                List<string> stds = db.Etudiants.Select(x => x.Name).ToList();
+                //List<string> stds = db.Etudiants.Select(x => x.Name).ToList();
 
-                foreach (var item in stds)
-                {
-                    Console.WriteLine(item);
-                }
-
-                Console.ReadLine();
+                //foreach (var item in stds)
+                //{
+                //    Console.WriteLine(item);
+                //}
             }
         }
     }
