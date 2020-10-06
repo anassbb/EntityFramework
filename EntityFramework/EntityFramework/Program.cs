9f89aaf9b821d6ed429d7084eed6d138f78b6265
anassbb <anasabassi11@gmail.com>
2020-10-06 23:42:36 +0200

    Patching avec CRUD

--- EntityFramework/EntityFramework/Program.cs
+++ EntityFramework/EntityFramework/Program.cs
@@ -12,9 +12,8 @@ namespace EntityFramework
         {            
             using (var db = new Models.TestDbContext())
             {
-                Prof1esseur prof = db.Professeurs.SingleOrDefault(x => x.PK == 3);              
-
-                db.Professeurs.Remove(prof);
+                db.Professeurs.Add(new Prof1esseur { Name = "a1", Email="a3@a3com", GPA = 2.3f });                
+                db.Professeurs.Add(new Prof1esseur { Name = "a2", Email = "a4@a4.com", GPA = 2.2f });
                 db.SaveChanges();
             }
         }
