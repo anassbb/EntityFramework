a12a2747432bfe9b4e40e70f2cf421071cd62a7c
anassbb <anasabassi11@gmail.com>
2020-10-06 11:40:02 +0200

    Test Unitaire

---
+++ EntityFramework/MathOperation/BasicsOperation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MathOperation
+{
+    public class BasicsOperation
+    {
+        public int Add(int x, int y) => x + y;
+        public int sub(int x, int y) => x - y;
+    }
+}
