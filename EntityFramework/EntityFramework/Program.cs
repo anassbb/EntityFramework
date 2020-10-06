9de2952793130b96520b609051a6f309e53737c3
anassbb <anasabassi11@gmail.com>
2020-10-06 23:57:20 +0200

    Transaction

--- EntityFramework/EntityFramework/Program.cs
+++ EntityFramework/EntityFramework/Program.cs
@@ -9,12 +9,29 @@ namespace EntityFramework
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
             using (var db = new Models.TestDbContext())
             {
-                db.Professeurs.Add(new Prof1esseur { Name = "a1", Email="a3@a3com", GPA = 2.3f });                
-                db.Professeurs.Add(new Prof1esseur { Name = "a2", Email = "a4@a4.com", GPA = 2.2f });
-                db.SaveChanges();
+                using (var trans = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Professeurs.Add(new Prof1esseur { Name = "helloYoyo", Email = "helloyoyo@gmail.com", GPA = 2.1f });
+                        db.SaveChanges();
+                        throw new Exception("yes");
+                        Prof1esseur p = db.Professeurs.SingleOrDefault(x => x.PK == 1);
+                        db.Professeurs.Remove(p);
+                        trans.Commit();
+                    }
+                    catch
+                    {
+                        trans.Rollback();
+                    }
+                    finally
+                    {
+                        trans.Dispose();
+                    }
+                }
             }
         }
     }
